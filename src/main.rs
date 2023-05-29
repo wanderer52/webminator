6c47859567ba81994165504cf26de84926ebb8d1
wanderer52 <134962539+wanderer52@users.noreply.github.com>
2023-05-29 20:41:14 +0100

    Add command line argument acquisition and validation functionality to get_thread_url.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use regex::Regex;
+use std::env;
+
+fn get_thread_url() -> Result<String, String> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        return Err(String::from(
+            "Please provide exactly one command line argument.",
+        ));
+    }
+
+    let re = Regex::new(r"https://boards\.(4chan|4channel)\.org/[[:alnum:]]+/thread/\d+");
 }
+
+fn main() {}
